8a2020ce4b0907fcdd2a86e04ea118b8e300c6d8
Aditya Chitta <achitta@umich.edu>
2019-11-04 20:03:01 -0500

    Added sub feature and check for bad input

--- rpn.py
+++ rpn.py
@@ -8,9 +8,16 @@ def calculate(arg):
 			num2 = stack.pop()
 			result = num1+num2
 			stack.append(result)
+		elif token == '-':
+			num1 = stack.pop()
+			num2 = stack.pop()
+			result = num2 - num1
+			stack.append(result)
 		else:
 			stack.append(int(token))
-	print(stack)
+		print(stack)
+	if len(stack) != 1:
+		raise TypeError("Bad Input")
 	return stack.pop()
 def main():
 	while True:
