b3f7c13ca0f91a0b5f4e3e2f89e1c5011307f601
Aditya Chitta <achitta@umich.edu>
2019-11-04 20:09:23 -0500

    Updated files

--- rpn.py
+++ rpn.py
@@ -1,27 +1,37 @@
 #!/usr/bin/env python3
 
-def calculate(arg):
-	stack = list()
-	for token in arg.split():
-		if token == '+':
-			num1 = stack.pop()
-			num2 = stack.pop()
-			result = num1+num2
-			stack.append(result)
-		elif token == '-':
-			num1 = stack.pop()
-			num2 = stack.pop()
-			result = num2 - num1
-			stack.append(result)
-		else:
-			stack.append(int(token))
-		print(stack)
-	if len(stack) != 1:
-		raise TypeError("Bad Input")
-	return stack.pop()
+import operator
+
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
+
 def main():
-	while True:
-		calculate(input('rpn calc> '))
+    while True:
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
 if __name__ == '__main__':
-	main()
+    main()
+
