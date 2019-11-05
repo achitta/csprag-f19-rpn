09b6f7807925bd0cbd0367bfe68f98980c0ee1ba
Aditya Chitta <achitta@umich.edu>
2019-11-04 19:51:15 -0500

    Implement add

--- rpn.py
+++ rpn.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
-	pass
-
+	stack = list()
+	for token in arg.split():
+		if token == '+':
+			num1 = stack.pop()
+			num2 = stack.pop()
+			result = num1+num2
+			stack.append(result)
+		else:
+			stack.append(int(token))
+	print(stack)
+	return stack.pop()
 def main():
 	while True:
 		calculate(input('rpn calc> '))
